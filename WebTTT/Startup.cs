a75653f77e89e3bb0a4d9989534e1e5f2fa9242d
Kaloyan Doychonov <kokolia1070@gmail.com>
2022-03-27 12:30:29 +0300

    [ADD] From PrevTTT

--- WebTTT/Startup.cs
+++ WebTTT/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,9 +13,17 @@ namespace WebTTT
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddRazorPages();
 			services.AddSignalR();
+			services.AddControllers();
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -25,10 +34,16 @@ namespace WebTTT
 			}
 
 			app.UseFileServer();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
 			app.UseRouting();
+
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
+				endpoints.MapRazorPages();
 				endpoints.MapHub<TTTHub>("/hub");
 				endpoints.MapGet("/getgameslist", async context =>
 				{
